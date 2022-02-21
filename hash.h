a8e1f2f4a8fd0e63eff79919beedd50428a3f4b3
Heiko Jappe <h.jappe@gmail.com>
2022-02-21 12:13:21 +0100

    fixes

--- hash.h
+++ hash.h
@@ -1,8 +1,8 @@
 #define HASHSIZE 100001
 
 struct hash_t {
-  struct hash_t  *next;
-  char           *str;
+    struct hash_t  *next;
+    char           *str;
 };
 
 extern struct hash_t *hash_lookup(char *str);
